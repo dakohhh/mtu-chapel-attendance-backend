c71cdf01f6bf0a37fe239377d0106b7c5fcab179
dakohhh <wiizzydreadmill@gmail.com>
2024-06-26 15:52:51 +0100

    feat: Add openpyxl dependency for Excel file handling - Modified Schema for gender field
    - Added 'does_matric_exist' query function

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ addExceptionHandlers(app)
 
 
 import pandas as pd
-from database.schema import Student,AcademicSession
+from database.schema import Student, AcademicSession
 
 
 @app.post("/test")
@@ -69,14 +69,12 @@ async def creat_test_students(request: Request):
     return True
 
 
-
-
-
 @app.post("/apply")
 async def apply_acdemic_session_students(request: Request):
 
-
-    academic_session_ = AcademicSession.objects(id=ObjectId("661bf99480564d2bf479ccac")).first()
+    academic_session_ = AcademicSession.objects(
+        id=ObjectId("661bf99480564d2bf479ccac")
+    ).first()
 
     students = Student.objects()
 
@@ -87,3 +85,60 @@ async def apply_acdemic_session_students(request: Request):
 
     return True
 
+
+import pandas as pd
+from database.choices import *
+from utils.resolve import resolve_dept, resolve_level, resolve_gender
+from bson import ObjectId
+from database.schema import AcademicSession
+
+@app.post("/create-mtu-script")
+async def create_script(request: Request):
+
+    data = pd.read_excel(
+        "./data/COMPREHENSIVE_LIST_OF_REGISTERED_STUDENTS_WITH_ALLOCATED_HOSTEL.xlsx"
+    )
+
+    data.drop(data.columns[-1], axis=1, inplace=True)
+
+    data.dropna(subset=["LEVEL", "MATRICULATION"], inplace=True)
+
+    data = data.drop_duplicates(subset=['MATRICULATION'], keep="first")
+
+    dept = {val: key for key, val in dict(DEPARTMENT_CHOICES).items()}
+
+    data["DEPT"] = data["DEPT"].apply(resolve_dept)
+
+    data["LEVEL"] = data["LEVEL"].apply(resolve_level)
+
+    data["GENDER"] = data["GENDER"].apply(resolve_gender)
+
+    # data.to_excel("./data/preprocessed.xlsx", index=False)
+
+    current_academic_session = AcademicSession.objects().first()
+
+    print(current_academic_session)
+
+    for index, row in data.iterrows():
+
+        names = str(row["OTHER NAMES"]).split(" ")
+
+        first_name = names[0]
+        other_name = names[1] if len(names) > 1 else None
+
+        print(row["S/N"])
+
+        student = Student(
+            firstname=first_name,
+            othername=other_name,
+            lastname=row["SURNAME"],
+            matric_no=row["MATRICULATION"],
+            level=row["LEVEL"],
+            department=row["DEPT"],
+            gender=row["GENDER"],
+            chapel_seat_number=random.randint(1, 35),
+            chapel_group_number=random.randint(1, 8),
+            academic_session=current_academic_session,
+        )
+
+        student.save()
